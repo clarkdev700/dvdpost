145e727ab59c675a264cf4d51dd2042dd8a5f074
Evans <clarkdev700@gmail.com>
2021-11-28 11:03:21 +0400

    create query for searching product

--- src/Repository/ProductRepository.php
+++ src/Repository/ProductRepository.php
@@ -67,7 +67,7 @@ class ProductRepository extends ServiceEntityRepository
 
     /**
       * @return Product[] Returns an array of Product objects
-      */
+     */
     
     public function findNewProduct($value)
     {
@@ -79,6 +79,45 @@ class ProductRepository extends ServiceEntityRepository
             ->getQuery()
             ->getResult()
         ;
+    }
+
+    function findProduct($motcle, $genre = null, $datedebut = null, $datefin = null) {
+        $conn = $this->getEntityManager()->getConnection();
+        
+        $parameters['motcle'] = $motcle;
+        
+        $q = "SELECT * FROM product WHERE title LIKE ':motcle%' or title LIKE '%:motcle%'";
+        $q1 = "SELECT * FROM product WHERE description LIKE '%:motcle%'";
+        $q2 = "SELECT * FROM product WHERE producer_id in (SELECT producer_id FROM producer WHERE name_producer LIKE '%:motcle%')";
+        $q3 = "SELECT * FROM product WHERE product_id in (SELECT product_id FROM product_actor pa, actor a WHERE a.actor_id = pa.actor_id AND a.name_actor LIKE '%:motcle%')";
+        
+        if (!empty($genre)) {
+            $q .= " AND category_id = ':categorie'";
+            $q1 .= " AND category_id = ':categorie'";
+            $q2 .= " AND category_id = ':categorie'";
+            $q3 .= " AND category_id = ':categorie'";
+            //set category parameter value
+            $parameters[] = $categorie;
+        }
+
+        if (!empty($datedebut)) {
+            $q .= " AND publication_date BETWEEN  :datedebut AND :datefin";
+            $q1 .= " AND publication_date BETWEEN :datedebut AND :datefin";
+            $q2 .= " AND publication_date BETWEEN :datedebut AND :datefin";
+            $q3 .= " AND publication_date BETWEEN :datedebut AND :datefin";
+            //set date debut and datefin parameters value
+            $parameters['datedebut'] = $datedebut;
+            $parameters['datefin'] = $datefin;
+        }
+
+
+        $sql = "$q UNION $q1 UNION $q2 UNION $q3";
+            
+        $stmt = $conn->prepare($sql);
+        $stmt->execute($parameters);
+
+        // returns an array of arrays (i.e. a raw data set)
+        return $stmt->fetchAllAssociative();        
     }
 
 }
